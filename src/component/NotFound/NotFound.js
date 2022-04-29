ee90e8b539cc95435585b2af10b7da1aa2aa63dd
Ezabulbary <ezabul.bary@gmail.com>
2022-04-29 15:36:34 +0600

    all component added

---
+++ src/component/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Your Search is Not Found</h1>
+            <h4>404</h4>
+        </div>
+    );
+};
+
+export default NotFound;
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAXukz8jcLKZlP-3rK3EJ11NUDUuBtNjw4",
+    authDomain: "fir-router-integration-f44aa.firebaseapp.com",
+    projectId: "fir-router-integration-f44aa",
+    storageBucket: "fir-router-integration-f44aa.appspot.com",
+    messagingSenderId: "671579898904",
+    appId: "1:671579898904:web:2e7eeb4ce2fd42c5876f6d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
